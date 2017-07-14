70182aedf37aefd143c9242349494849404766f8
ThomasElkins <elkins.thomas@gmail.com>
2017-07-14 14:25:18 -0700

    style no function

--- app.js
+++ app.js
@@ -25,11 +25,21 @@ $('#clearTracksButton').append('<div id="chooseTracksButtonText"><h3>clear track
 $('#playlistButtonContainer').append('<div id="submitBinButton">')
 $('#submitBinButton').append('<div id="submitBinButtonText"><h3>submit bin</h3></div>').css("color", "white",)
 
+// // global data
+// var getData = $.get("https://lit-fortress-6467.herokuapp.com/object");
+// var allData = []
+// getData.done(function(e) {
+//   if (getData.status !== 200) return;
+//   for (var i = 0; i < e["results"].length; i++) {
+//       allData.push(e["results"][i]);
+//   }
+// })
+
 // Get request on homepage
 
+var splashArt = [];
 $.get("https://lit-fortress-6467.herokuapp.com/object", function(data){
   var albumArr = data["results"];
-  var splashArt = [];
     for (var i = 0; i < albumArr.length; i++){
       splashArt.push(albumArr[i]["cover_art"])
     }
@@ -43,11 +53,11 @@ $.get("https://lit-fortress-6467.herokuapp.com/object", function(data){
     }
     return array;
 }
+// Add random images over railroad image
 var shuffledArt = shuffleArray(splashArt);
 var splashArtOne = shuffledArt[0];
 var splashArtTwo = shuffledArt[1];
 var splashArtThree = shuffledArt[2];
-console.log(splashArtOne)
 $('.transparentImgContainer').append('<img id="splashOne" src="images/'+splashArtOne+'"/>')
 $('.transparentImgContainer').append('<img id="splashTwo" src="images/'+splashArtTwo+'"/>')
 $('.transparentImgContainer').append('<img id="splashThree" src="images/'+splashArtThree+'"/>')
@@ -55,6 +65,21 @@ $('.transparentImgContainer').append('<img id="splashThree" src="images/'+splash
 //End get request
 })
 
+// Second page album population
+$.get("https://lit-fortress-6467.herokuapp.com/object", function(e){
+  var results = e["results"];
+    for (var i = 0; i < results.length; i++){
+      var albumID = results[i]["id"]
+      var art = results[i]["cover_art"]
+      $('#albumNavBar').append('<div id="'+albumID+'" class="artWrapper"</div>')
+      $('#'+albumID+'').append('<img src="images/'+art+'" class="albums"/>')
+    }
+// Add album to list on click
+  $('.artWrapper').click(function(){
+    var clickedOn = this.id
+    console.log(e["results"])
+  })
+})
 
 
 //End load function
